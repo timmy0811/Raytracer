899316eb740ae2b0c794e47ae741daa85995b930
Timm <timmschmidt7@t-online.de>
2022-08-14 20:19:47 +0200

    Image format factor on resize

--- Raytracing/src/Renderer.cpp
+++ Raytracing/src/Renderer.cpp
@@ -7,7 +7,9 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 
 	return 0xff000000 | ( g << 8 ) | r;*/
 
-	glm::vec3 rayOrig(0.f, 0.f, 2.f);
+	glm::vec3 sphere(0.f, 0.f, 0.f);
+
+	glm::vec3 rayOrig(0.f, 0.f, 1.f);
 	glm::vec3 rayDir(coord.x, coord.y, -1.f);
 	rayDir = glm::normalize(rayDir);
 	float radius = 0.5f;
@@ -17,12 +19,12 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 	float a = glm::dot(rayDir, rayDir);
 
 	// b = 2axbx + 2ayby + 2azbz - bxa - byb - bzc
-	//float b = 2 * (rayPos.x * rayDir.x + rayPos.y * rayDir.y + rayPos.z * rayDir.z) - rayDir.x * position.x - rayDir.y * position.y - rayDir.z * position.z;
-	float b = 2.f * glm::dot(rayOrig, rayDir);
+	float b = 2 * glm::dot(rayOrig, rayDir) - rayDir.x * sphere.x - rayDir.y * sphere.y - rayDir.z * sphere.z;
+	//float b = 2.f * glm::dot(rayOrig, rayDir);
 
 	// c = ax^2 + ay^2 + az^2 - axa - ayb - azc - r^2
-	//float c = pow(rayPos.x, 2) + pow(rayPos.y, 2) + pow(rayPos.z, 2) - rayPos.x * position.x - rayPos.y * position.y - rayPos.z * position.z;
-	float c = glm::dot(rayOrig, rayOrig) - pow(radius, 2.f);
+	float c = glm::dot(rayOrig, rayOrig) - pow(radius, 2.f) - rayOrig.x * sphere.x - rayOrig.y * sphere.y - rayOrig.z * sphere.z;
+	//float c = glm::dot(rayOrig, rayOrig) - pow(radius, 2.f);
 
 	// Determine solutions
 	float disc = pow(b, 2) - 4 * a * c;
@@ -36,6 +38,15 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 		p.x = rayOrig.x + rayDir.x * lowerT;
 		p.y = rayOrig.y + rayDir.y * lowerT;
 		p.z = rayOrig.z + rayDir.z * lowerT;
+
+		float growthZ = abs(- ((p.x - sphere.x) / std::sqrt(-pow(p.x - sphere.x, 2.f) - pow(p.y - sphere.y, 2.f) + radius * radius)));
+		float growthY = abs(-((p.x - sphere.x) / std::sqrt(-pow(p.x - sphere.x, 2.f) - pow(p.z - sphere.z, 2.f) + radius * radius)));
+
+		if (growthZ >= 1) growthZ = 1;
+		if (growthY >= 1) growthY = 1;
+
+		uint8_t b = 255 * growthZ;
+		uint8_t r = 255 * growthY;
 
 		return 0xff00ff00;
 		//return p;
@@ -72,9 +83,11 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 
 void Renderer::Render()
 {
+	float formatFac = (float)m_ImageRendered->GetWidth() / (float)m_ImageRendered->GetHeight();
 	for (register uint32_t y = 0; y < m_ImageRendered->GetHeight(); y++) {
 		for (register uint32_t x = 0; x < m_ImageRendered->GetWidth(); x++) {
 			glm::vec2 coord = { x / (float)m_ImageRendered->GetWidth(), y / (float)m_ImageRendered->GetHeight() };
+			coord.x *= formatFac;
 			coord = coord * 2.f - 1.f;
 			m_ImageData[x + y * m_ImageRendered->GetWidth()] = PerPixel(coord);
 		}
